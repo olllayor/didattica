d860be998f977236f3ce603969030415b2f33f9e
Olloyor Makhammatnabiyev <muhammadnabiyevolloyor@gmail.com>
2024-07-22 17:57:51 +0500

    changed chat ui[more bugs]

--- config/settings.py
+++ config/settings.py
@@ -14,12 +14,11 @@ import os
 from pathlib import Path
 
 
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-TEMPATE_DIR = os.path.join(BASE_DIR, 'templates')
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+TEMPATE_DIR = os.path.join(BASE_DIR, "templates")
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
@@ -30,26 +29,33 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-SECRET_KEY = 'django-insecure-$%e5_12d&f)u9x&k18p6m!l*mrl#sb51)3$ms+n#usd9c7$wh('
+SECRET_KEY = "django-insecure-$%e5_12d&f)u9x&k18p6m!l*mrl#sb51)3$ms+n#usd9c7$wh("
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 
 # ALLOWED_HOSTS = ["didattica.jprq.app", '127.0.0.1', "demo.jprq.app"]
-ALLOWED_HOSTS = ['98.70.77.175', 'didattica.uz', '.vercel.app', '127.0.0.1', 'localhost', '.jprq.app']
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+ALLOWED_HOSTS = [
+    "98.70.77.175",
+    "didattica.uz",
+    ".vercel.app",
+    "127.0.0.1",
+    "localhost",
+    ".jprq.app",
+]
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 
 CSRF_TRUSTED_ORIGINS = [
-    'https://didattica.uz',
-    'https://www.didattica.uz',
-    'https://98.70.77.175',
-    'http://didattica.uz',
-    'http://www.didattica.uz',
-    'http://98.70.77.175',
-    'https://localhost',
-    'htpp://127.0.0.1',
-    'https://demo.jprq.app',
+    "https://didattica.uz",
+    "https://www.didattica.uz",
+    "https://98.70.77.175",
+    "http://didattica.uz",
+    "http://www.didattica.uz",
+    "http://98.70.77.175",
+    "https://localhost",
+    "htpp://127.0.0.1",
+    "https://demo.jprq.app",
 ]
 
 CSRF_COOKIE_SECURE = True
@@ -58,54 +64,54 @@ CSRF_COOKIE_SECURE = True
 # Application definition
 
 INSTALLED_APPS = [
-    'widget_tweaks',
+    "widget_tweaks",
     "debug_toolbar",
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'students',
-    'teachers',
-    'exams',
-    'chat',
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "students",
+    "teachers",
+    "exams",
+    "chat",
 ]
 
 MIDDLEWARE = [
     "debug_toolbar.middleware.DebugToolbarMiddleware",
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 # CSRF_COOKIE_SECURE = False
-ROOT_URLCONF = 'config.urls'
-MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
+ROOT_URLCONF = "config.urls"
+MESSAGE_STORAGE = "django.contrib.messages.storage.session.SessionStorage"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [TEMPATE_DIR],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [TEMPATE_DIR],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
-            'libraries': {
-                'custom_filters': 'chat.custom_filters',
+            "libraries": {
+                "custom_filters": "chat.custom_filters",
             },
         },
     },
 ]
 
-WSGI_APPLICATION = 'config.wsgi.application'
+WSGI_APPLICATION = "config.wsgi.application"
 
 
 # Database
@@ -122,10 +128,10 @@ WSGI_APPLICATION = 'config.wsgi.application'
 #     }
 # }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'database', # This is where you put the name of the db file.
-                 # If one doesn't exist, it will be created at migration time.
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": "database",  # This is where you put the name of the db file.
+        # If one doesn't exist, it will be created at migration time.
     }
 }
 
@@ -134,16 +140,16 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
 
@@ -151,9 +157,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = 'Asia/Tashkent'
+TIME_ZONE = "Asia/Tashkent"
 
 USE_I18N = True
 
@@ -163,21 +169,21 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_DIR = os.path.join(BASE_DIR, 'static')
+STATIC_URL = "/static/"
+STATIC_DIR = os.path.join(BASE_DIR, "static")
 
 # Additional locations of static files
 STATICFILES_DIRS = [
     STATIC_DIR,
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "/"
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 # INTERNAL_IPS = [
 #     "127.0.0.1",
 # ]
--- students/models.py
+++ students/models.py
@@ -1,25 +1,32 @@
 from django.db import models
 from django.contrib.auth.models import User
+
 # Create your models here.
+
 
 class Student(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     student_id = models.CharField(max_length=15, unique=True)
-    profile_pic = models.ImageField(upload_to='profile_pics/Students', null=True, blank=True)
+    profile_pic = models.ImageField(
+        upload_to="profile_pics/Students", null=True, blank=True
+    )
     mobile = models.CharField(max_length=20, null=False)
+
     @property
     def get_name(self):
-        return self.user.first_name + ' ' + self.user.last_name
+        return self.user.first_name + " " + self.user.last_name
+
     @property
     def get_institute(self):
         return self
+
     @property
     def username(self):
         return self.user.username
+
     @property
     def get_mobile(self):
         return self.mobile
-    
+
     def __str__(self):
         return self.user.first_name
-    
